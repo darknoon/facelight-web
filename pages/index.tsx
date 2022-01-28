6751b4fda38380c3b2fc9ef5fdcfc50937672ca4
Andrew Pouliot <andrew@darknoon.com>
2022-01-28 11:54:13 -0500

    Change colors, add video, maybe fix build. let's go\!

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,18 +2,23 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import Script from "next/script";
 
-const VideoPlayer = () => (
+const chaos =
+  "https://player.vimeo.com/video/671208512?h=c8e89d1b24&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479";
+const ui =
+  "https://player.vimeo.com/video/671219052?h=80261ada6e&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479";
+const VideoPlayer = ({ url }: { url: string }) => (
   <>
     <iframe
       className={styles.video}
-      src="https://player.vimeo.com/video/671208512?h=c8e89d1b24&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
+      src={url}
       frameBorder="0"
       allow="autoplay; fullscreen; picture-in-picture"
       allowFullScreen
       title="FaceLight"
     ></iframe>
-    <script src="https://player.vimeo.com/api/player.js"></script>
+    {/* <Script src="https://player.vimeo.com/api/player.js"></Script> */}
   </>
 );
 
@@ -43,15 +48,18 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>FaceLight</h1>
-
         <p className={styles.description}>
           Turn your mac's XDR screen into a video light!
         </p>
-
-        <VideoPlayer />
-
+        <VideoPlayer url={chaos} />
+        <p></p>
+        <p>
+          This is what the UI looks like (screen recording doesn't show how darn
+          bright it is)
+        </p>
+        <VideoPlayer url={ui} />
+        <p>Would you like to try it?</p>
         <Icon />
-
         <p>
           Works with MacBook Pro with M1 Pro or Pro Max, or any Mac connected to
           a Pro Display XDR.
